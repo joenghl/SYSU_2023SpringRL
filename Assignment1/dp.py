a8bee3aa4add389c4be179808121ccb84eb00cb1
Yang Hanlin <haringyang@163.com>
2023-03-28 14:17:06 +0800

    init

--- Assignment1/dp.py
+++ Assignment1/dp.py
@@ -1,3 +1,39 @@
+import copy
 import numpy as np
 from env.grid_scenarios import MiniWorld
 
+
+# Hypar-parameters that could be helpful.
+GAMMA = 0.9
+EPSILON = 0.001
+BLOCKS = [14, 15, 21, 27]
+R = [
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, -1,
+    0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+]
+
+
+def policy_iteration():
+    pass
+
+
+def value_iteration():
+    pass
+
+
+if __name__ == "__main__":
+    env = MiniWorld()
+    n_state = env.observation_space.n
+    n_action = env.action_space.n
+
+    ######################################################
+    # write your code to get a convergent value table v. #
+    ######################################################
+
+    env.update_r(v)
+    for _ in range(2000):
+        env.render()
