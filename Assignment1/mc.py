3285627b66d5299acc0f6293138b2ce85627ece9
Yang Hanlin <haringyang@163.com>
2023-03-28 14:34:04 +0800

    init commit

--- Assignment1/mc.py
+++ Assignment1/mc.py
@@ -1,3 +1,17 @@
 import numpy as np
 from env.grid_scenarios import MiniWorld
 
+
+GAMMA = 0.9
+EPSILON = 0.001
+LR = 0.001
+BLOCKS = [14, 15, 21, 27]
+
+
+def monte_carlo(env):
+    pass
+
+
+if __name__ == "__main__":
+    env = MiniWorld()
+    monte_carlo(env)
--- Assignment1/td.py
+++ Assignment1/td.py
@@ -1,3 +1,17 @@
 import numpy as np
 from env.grid_scenarios import MiniWorld
 
+
+GAMMA = 0.9
+EPSILON = 0.001
+LR = 0.001
+BLOCKS = [14, 15, 21, 27]
+
+
+def temporal_difference(env):
+    pass
+
+
+if __name__ == "__main__":
+    env = MiniWorld()
+    temporal_difference(env)
